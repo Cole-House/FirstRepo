3c7936e44759b5884376f29f5aca4c01ecc9e130
guacole <30875198+guacole@users.noreply.github.com>
2017-08-14 08:54:44 -1000

    Start of JS
    August 14th

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello(){
+  //I create a function called sayHello//
+  var userName = prompt("What is your name?");
+  //variable called userName and have them answer//
+  window.alert("Hello " + userName);
+  //say hello to userName//
+}
