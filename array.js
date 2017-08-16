d03d0d1ea0c35ba76427ab29627b8c2f04b874f5
guacole <30875198+guacole@users.noreply.github.com>
2017-08-16 08:42:31 -1000

    Array practice

---
+++ array.js
@@ -0,0 +1,6 @@
+var favFoods = ["ham", "lettuce", "takis"]
+//I set up an array for foods 0-2//
+var students = ["Michael", "Raene", "Chuck", "Ryan"]
+//array of student names 0-3//
+window.alert(students[2]+ " " + favFoods[0])
+//popup that calls upon Chuck + Ham//
