e9dc875c291a63f73c3499239758f8831019c813
guacole <30875198+guacole@users.noreply.github.com>
2017-08-16 09:24:40 -1000

    simple multiplier

---
+++ ArithmeticOperator.js
@@ -0,0 +1,8 @@
+var number = prompt("What is your favorite number?");
+//asked user for first #//
+var numberdos = prompt("Your second fav number?");
+//asked user for second #//
+var calculation = [(number)*(numberdos)];
+// multiplied both numbers with Arithmetic operator//
+window.alert(calculation);
+//popped up answer aka calculation//
